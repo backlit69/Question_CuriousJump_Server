0c165b757e440c24921c5d9d91ecc9188e6c0d72
Backlit <saket9max@gmail.com>
2024-05-08 10:31:49 +0530

    b

--- index.js
+++ index.js
@@ -40,17 +40,17 @@ const db = getFirestore();
   app.use(function (req, res, next) {
 
     // Website you wish to allow to connect
-    res.setHeader('Access-Control-Allow-Origin', 'https://question-curious-jump-client.vercel.app');
+    res.setHeader("Access-Control-Allow-Origin", "https://question-curious-jump-client.vercel.app");
 
     // Request methods you wish to allow
-    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+    res.setHeader("Access-Control-Allow-Methods", "GET, POST, OPTIONS, PUT, PATCH, DELETE");
 
     // Request headers you wish to allow
-    res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
+    res.setHeader("Access-Control-Allow-Headers", "X-Requested-With,content-type");
 
     // Set to true if you need the website to include cookies in the requests sent
     // to the API (e.g. in case you use sessions)
-    res.setHeader('Access-Control-Allow-Credentials', true);
+    res.setHeader("Access-Control-Allow-Credentials", "true");
 
     // Pass to next layer of middleware
     next();
